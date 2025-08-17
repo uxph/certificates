ade0c6aa63dec170d063cee1e73eda3a03e66eac
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2025-08-17 14:21:07 +0800

    change slots color scheme

--- src/components/WorkshopRegistration/WorkshopCard.jsx
+++ src/components/WorkshopRegistration/WorkshopCard.jsx
@@ -56,10 +56,10 @@ const WorkshopCard = ({ workshop, blockName, isSelected, onSelect }) => {
               <div
                 className={`px-2 py-1 rounded text-xs font-semibold ${
                   workshop.slotsLeft === 0
-                    ? "bg-red-500/20 text-red-400 border border-red-500/30"
+                    ? "bg-red-500/20 text-red-600 border border-red-500/30"
                     : workshop.slotsLeft <= 5
-                    ? "bg-yellow-500/20 text-yellow-400 border border-yellow-500/30"
-                    : "bg-green-500/20 text-green-400 border border-green-500/30"
+                    ? "bg-yellow-500/20 text-yellow-600 border border-yellow-500/30"
+                    : "bg-green-500/20 text-green-600 border border-green-500/30"
                 }`}
               >
                 {workshop.slotsLeft === 0
