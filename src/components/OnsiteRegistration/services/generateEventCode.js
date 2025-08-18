b3c0920be8ae95b7c8b1360bd37271ece32968e2
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2025-08-19 00:11:29 +0800

    pad last name part in generating event codes

--- src/components/OnsiteRegistration/services/generateEventCode.js
+++ src/components/OnsiteRegistration/services/generateEventCode.js
@@ -1,7 +1,7 @@
 function generateEventCode(eventId, lastName) {
     // Ensure we always have at least 3 characters
     const evPart = (eventId || "EVT").substring(0, 3).toUpperCase();
-    const lnPart = (lastName || "XXX").substring(0, 3).toUpperCase();
+    const lnPart = (lastName || "XXX").substring(0, 3).toUpperCase().padEnd(3, "X");
 
     // Generate numeric sections
     //    const num1 = String(Math.floor(Math.random() * 1_000_000)).padStart(6, "0");
