2f588788c463c4729e7ee169e49003cf99c8089a
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2025-08-20 21:22:09 +0800

    add password protection to csv upload

--- src/app/csv-upload/page.js
+++ src/app/csv-upload/page.js
@@ -1,6 +1,12 @@
 import PasswordProtection from "@/components/PasswordProtection";
 import CSVUpload from "@/components/CSVUpload";
+import { checkAuth } from "../actions/auth";
 
-export default function CSVUploadPage() {
+export default async function CSVUploadPage() {
+  const isAuthenticated = await checkAuth();
+  if (!isAuthenticated) {
+    return <PasswordProtection />;
+  }
+
   return <CSVUpload />;
 }
